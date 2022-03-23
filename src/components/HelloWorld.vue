501aedd3b9ccf92cf85a0cc648ef3cadf9d93dcc
jiaolina <1451758123@qq.com>
2022-03-23 17:54:53 +0800

    feat：vue3基础练习（组件引入、watchEffect侦听器、ref Attribute绑定）

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,43 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted ,watchEffect} from "vue";
+import TodoItem from './TodoItem.vue'
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
+let isGreen = ref(false);
+let list = ref([1,2,3,4,5]);
+let todo=ref({text:123})
+
+function addClick() {
+  return count.value++;
+}
+/**
+ * Attribute绑定
+ */
+function changeColor() {
+  isGreen.value = !isGreen.value;
+}
+
+watchEffect(()=>{
+  if(count.value%2===0){
+    todo.value.text=count.value
+  }
+  console.log('监听中。。。',todo.value.text)
+})
+
+onMounted(() => {
+  console.log("【onMounted】count:" + count.value);
+});
+
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h1 :class="{ green: isGreen, h1: true }" @click="changeColor">{{ msg }}</h1>
+  <div class="mapArray"><span v-for="item in list">{{item+'-'}}</span></div>
+  <button type="button" @click="addClick">count is: {{ count }}</button>
+  <TodoItem :todo="todo"></TodoItem>
 </template>
 
 <style scoped>
@@ -48,5 +55,12 @@ code {
   padding: 2px 4px;
   border-radius: 4px;
   color: #304455;
+}
+
+.h1 {
+  cursor: pointer;
+}
+.green {
+  color: green;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: jiaolina
+ * @Date: 2022-03-23 15:28:25
+ * @LastEditors: jiaolina
+ * @LastEditTime: 2022-03-23 17:38:50
+-->
+<script setup lang="ts"> 
+  import {ref} from 'vue'
+  const props=defineProps({
+    todo:Object
+  })
+
+</script>
+
+<template >
+  <div>第{{props.todo?.text}}个item</div>
+</template>
+
+<style lang="scss" scoped></style>
