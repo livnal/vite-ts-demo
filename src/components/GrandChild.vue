eaa6409031ecd2368837fdf646b1982f2234604e
jiaolina <1451758123@qq.com>
2022-04-22 10:20:39 +0800

    feat：添加“@”路径别名配置，安装@type/node依赖

--- src/components/GrandChild.vue
+++ src/components/GrandChild.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, useAttrs, onMounted, inject, watchEffect, watch } from 'vue'
-import { useMouse } from '../utils/mouse.js'
+import { useMouse } from '@/utils/mouse'
 
 // 这里的透传attrs并不是响应式的，不能通过侦听器去监听它的变化；
 // 如果需要响应性，可以使用prop，或者使用onUpdated()使得在每次更新时结合最新的attrs执行副作用
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(
-  //   {
-  //   refTransform: true
-  // }
-  )],
-  server:{
-    host:"0.0.0.0"
-  }
+  plugins: [
+    vue(),
+    //   {
+    //   refTransform: true
+    // }
+  ],
+  server: {
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // 路径别名
+    },
+    extensions: ['.js', '.json', '.ts'], // vite 官方文档中 不建议忽略 .vue 后缀的文，所以在 import 引入文件的时候需要加 .vue
+  },
 })
